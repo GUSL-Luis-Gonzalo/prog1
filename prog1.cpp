fbcdc9d5a0489592033fe5da10f28a4fe18dd0f3
Luis Gonzalo Guzman Sanchez <105834318+GUSL-Luis-Gonzalo@users.noreply.github.com>
2022-05-18 23:07:15 -0500

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,17 @@
+/* Autor: Luis Gonzalo Guzman Sanchez, Realizado: 03/02/2022 
+	Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es el primer programa de Lenguaje C de la materia de Programacion Estructurada
+Muestra el uso de:
+-printf para mostrar los mensajes.
+-Comentarios. para la documentacion interna del programa.
+	*/
+#include <stdio.h>
+
+int main() {
+/* Para imprimir */	printf("Hola Mundo!!!\n"); 
+printf("Luis Gonzalo Guzman Sanchez");
+	return 0;
+}
